13044afaccc21f3034d3ba1e8953efc8de2c6519
pax <91659503+patrik-16@users.noreply.github.com>
2023-06-06 15:06:35 +0200

    Update 2 GameSessionApplicationTests.java

--- GameSession/src/test/java/com/escapedoom/gamesession/GameSessionApplicationTests.java
+++ GameSession/src/test/java/com/escapedoom/gamesession/GameSessionApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class GameSessionApplicationTests {
 
     @Test
-    void alwaysTrueTest() {
+    void contextLoads() {
         assertTrue(true);
     }
 
