8fff82cbbbc7bb041c0715e1112bf8770ceb1585
pax <91659503+patrik-16@users.noreply.github.com>
2023-06-06 15:25:12 +0200

    test ohne springboot annotation (nicht die ganze application starten)

--- GameSession/src/test/java/com/escapedoom/gamesession/GameSessionApplicationTests.java
+++ GameSession/src/test/java/com/escapedoom/gamesession/GameSessionApplicationTests.java
@@ -2,9 +2,9 @@ package com.escapedoom.gamesession;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+//@SpringBootTest
 class GameSessionApplicationTests {
    
     @Test
