3dc38a2eb4f3a263c1d5806b9a14fe872b8f746f
pax <91659503+patrik-16@users.noreply.github.com>
2023-06-24 22:49:33 +0200

    deployz.sh file update

--- deployz.sh
+++ deployz.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sshpass -p 'c1scod1sco' ssh -o StrictHostKeyChecking=no csdc@10.0.0.201 'pwsh -File ~/deployyyz/deployz.ps1'
+sshpass -p 'c1scod1sco' ssh -o StrictHostKeyChecking=no csdc@10.0.0.201 'docker kill $(docker ps -q)'
+sshpass -p 'c1scod1sco' ssh -o StrictHostKeyChecking=no csdc@10.0.0.201 'pwsh -File ~/deployyyz/deployz.ps1 &'
