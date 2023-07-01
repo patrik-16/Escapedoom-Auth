44f2bfec535f80fc5188e48c3b09c6a3b8fca5e2
pax <91659503+patrik-16@users.noreply.github.com>
2023-07-01 22:38:00 +0200

    balloon king

--- GameSession/src/main/java/com/escapedoom/gamesession/services/PlayerStateManagementService.java
+++ GameSession/src/main/java/com/escapedoom/gamesession/services/PlayerStateManagementService.java
@@ -76,7 +76,7 @@ public class PlayerStateManagementService {
         add("Ice");
         add("Phoenix");
         add("Ghost");
-        add("Dragon");
+        add("Balloon");
     }};
 
     private ArrayList<String> secondNames = new ArrayList<>() {{
@@ -98,7 +98,7 @@ public class PlayerStateManagementService {
         add("Rider");
         add("Bringer");
         add("Reaper");
-        add("Slayer");
+        add("King");
     }};
 
     private final Random random = new Random();
